21b4fcb9c1b3f79cc01ecf9dec8dbcbc246c994f
chocopie777 <egoran99@gmail.com>
2023-09-28 15:32:19 +0500

    Manipulating the DOM with Refs

---
+++ src/Manipulating the DOM with Refs/App.js
@@ -0,0 +1,79 @@
+import React, {useRef} from 'react';
+
+// const App = (props) => {
+//     const firstCatRef = useRef(null);
+//     const secondCatRef = useRef(null);
+//     const thirdCatRef = useRef(null);
+//
+//     function handleScrollToFirstCat() {
+//         firstCatRef.current.scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'nearest',
+//             inline: 'center'
+//         });
+//     }
+//
+//     function handleScrollToSecondCat() {
+//         secondCatRef.current.scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'nearest',
+//             inline: 'center'
+//         });
+//     }
+//
+//     function handleScrollToThirdCat() {
+//         thirdCatRef.current.scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'nearest',
+//             inline: 'center'
+//         });
+//     }
+//
+//     return (
+//         <>
+//             <nav>
+//                 <button onClick={handleScrollToFirstCat}>
+//                     Tom
+//                 </button>
+//                 <button onClick={handleScrollToFirstCat}>
+//                     asdd
+//                 </button>
+//                 <button onClick={handleScrollToFirstCat}>
+//                     asdsadsad
+//                 </button>
+//             </nav>
+//             <div>
+//                 <ul>
+//                     <li>
+//                         <img
+//                             src="https://placekitten.com/g/200/200"
+//                             alt="Tom"
+//                             ref={firstCatRef}
+//                         />
+//                     </li>
+//                     <li>
+//                         <img
+//                             src="https://placekitten.com/g/300/200"
+//                             alt="Maru"
+//                             ref={secondCatRef}
+//                         />
+//                     </li>
+//                     <li>
+//                         <img
+//                             src="https://placekitten.com/g/250/200"
+//                             alt="Jellylorum"
+//                             ref={thirdCatRef}
+//                         />
+//                     </li>
+//                 </ul>
+//             </div>
+//         </>
+//     )
+// };
+
+
+const App = (props) => {
+
+};
+
+export default App;
